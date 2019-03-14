e5b8c8cd3bdf351b8da44ed2c8c8fec058fa8cee
mzxl1987 <448632824@qq.com>
2019-03-14 11:26:24 +0800

    1.添加 circuit-breaker 2.添加config.properties,消除部分启动警告                add         朱露露

--- springCloud_web/src/main/java/com/miicrown/controller/TestController.java
+++ springCloud_web/src/main/java/com/miicrown/controller/TestController.java
@@ -1,16 +1,32 @@
 package com.miicrown.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import com.miicrown.service.TestService;
 
 //@RestController
+@EnableCircuitBreaker
 @Controller
 @RequestMapping(path="/test")
 public class TestController {
 
 	private static String helloAnyone = "Hello, %s !";
+	
+	@Autowired
+	private TestService testService;
+	
+	@Bean
+	public RestTemplate rest(RestTemplateBuilder builder){
+		return builder.build();
+	}
 	
 	@RequestMapping(path="/hello/{name}")
 	@ResponseBody
@@ -23,6 +39,12 @@ public class TestController {
 	public Object hello(){
 		System.out.println("Hello Page");
 		return "hello";
+	}
+	
+	@RequestMapping(path="/toRead")
+	@ResponseBody
+	public Object toRead(){
+		return testService.readingList(); 
 	}
 	
 	
---
+++ springCloud_web/src/main/java/com/miicrown/service/TestService.java
@@ -0,0 +1,30 @@
+package com.miicrown.service;
+
+import java.net.URI;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class TestService {
+	
+	private final RestTemplate restTemplate;
+	
+	public TestService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
+	@HystrixCommand(fallbackMethod="reliable")
+	public String readingList(){
+		URI url = URI.create("http://localhost:8081/miicrown/test/hello/JackAndTom");
+		
+		return this.restTemplate.getForObject(url, String.class);
+	}
+	
+	public String reliable(){
+		return "Error:URL can not reach!";
+	}
+	
+}
