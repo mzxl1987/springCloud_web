8c91d553f7ade84a408aef6d05f33181cf9142a2
mzxl1987 <448632824@qq.com>
2019-04-12 17:00:24 +0800

    1.Kafka 消费者和生产者的添加 2.Kafka默认服务端口9092 3.添加配置文件选择                         add          朱露露

---
+++ springCloud_web/src/main/java/com/miicrown/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,30 @@
+package com.miicrown.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableKafka
+public class KafkaConsumer {
+	
+	private Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+	
+	@KafkaListener(topics="test")
+	public void listenerTest(ConsumerRecord<?, ?> record){
+		
+		log.info("Kafka接收消息 : TOPIC [ TEST ]   :  MSG [ {} ]",record.value());
+		
+	}
+	
+	@KafkaListener(topics="chat")
+	public void listenerChat(ConsumerRecord<?, ?> record){
+		
+		log.info("Kafka接收消息 : TOPIC [ CHAT ]   :  MSG [ {} ]",record.value());
+		
+	}
+	
+}
---
+++ springCloud_web/src/main/java/com/miicrown/kafka/producer/KafkaProducer.java
@@ -0,0 +1,53 @@
+package com.miicrown.kafka.producer;
+
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@Component
+@EnableScheduling
+@EnableKafka
+public class KafkaProducer {
+
+	private Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+	
+	@Autowired
+	private KafkaTemplate kafkaTemplate;
+	
+	@Scheduled(cron=" 00/1 * * * * ?")
+	public void sendTestMsg(){
+		
+		String message = UUID.randomUUID().toString();
+		ListenableFuture future = kafkaTemplate.send("test", message);
+		
+		future.addCallback(succ -> {
+			log.info("Test消息发送成功");
+		}, fail -> {
+			log.error("消息发送失败");
+		});
+		
+	}
+	
+	@Scheduled(cron=" 00/5 * * * * ?")
+	public void sendChatMsg(){
+		
+		String message = UUID.randomUUID().toString();
+		ListenableFuture future = kafkaTemplate.send("chat", message);
+		
+		future.addCallback(succ -> {
+			log.info("Chat消息发送成功");
+		}, fail -> {
+			log.error("消息发送失败");
+		});
+		
+	}
+	
+}
