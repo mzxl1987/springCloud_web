754de87014c2881337fe780c4aa83f8c1dfc4135
mzxl1987 <448632824@qq.com>
2019-06-13 18:05:07 +0800

    解决WebSocket & Schedule同时使用冲突的问题               add       朱露露

--- springCloud_web/src/main/java/com/miicrown/config/BeanConfig.java
+++ springCloud_web/src/main/java/com/miicrown/config/BeanConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.client.RestTemplate;
 
 import com.miicrown.netty.server.EchoServer;
@@ -25,5 +27,17 @@ public class BeanConfig {
 	public RestTemplate restTemplate(){
 		return new RestTemplateBuilder().build();
 	}
+	
+	/**
+	 * 解决WebSocket & Scheduler同时使用冲突的问题
+	 * @return
+	 */
+	@Bean
+    public TaskScheduler taskScheduler(){
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(10);
+        taskScheduler.initialize();
+        return taskScheduler;
+    }
 	
 }
